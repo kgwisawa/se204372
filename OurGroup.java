f0f9d440f86ba20869baf0fc6e059cb7832b70c2
kgwisawa <98408096+kgwisawa@users.noreply.github.com>
2022-01-26 15:21:45 +0700

    add

--- OurGroup.java
+++ OurGroup.java
@@ -6,7 +6,7 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
-        groupMembers.add("YOUR NAME"); //PUT YOUR NAME HERE
+        groupMembers.add("Wisawa Chotiphanpong 6220500725"); //PUT YOUR NAME HERE
         groupMembers.add("Trirattree Koanamsai 6220503210");
         groupMembers.add("Nakrob Sukjai 6220502141");
     }
