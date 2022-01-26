2a880f6e4795f43673556937a54ec2bb42e135e3
PedklahZa007 <91517441+PedklahZa007@users.noreply.github.com>
2022-01-26 15:33:08 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -9,6 +9,8 @@ public class OurGroup {
         groupMembers.add("Wisawa Chotiphanpong 6220500725"); //PUT YOUR NAME HERE
         groupMembers.add("Trirattree Koanamsai 6220503210");
         groupMembers.add("Nakrob Sukjai 6220502141");
+        groupMembers.add("Pedklah Kamolsukyunyong 6220504721");
+
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
