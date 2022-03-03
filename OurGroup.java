7d304b2ca5818a5b6cf9c12c5f66415efb6af172
kgwisawa <wisawa.cho@ku.th>
2022-03-03 22:26:55 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -7,7 +7,7 @@ public class OurGroup {
     {
         groupMembers = new Vector<String>();
         groupMembers.add("Wisawa Chotiphanpong 6220500725"); //PUT YOUR NAME HERE
-        groupMembers.add("Trirattree Koanamsai 6220503210");
+        groupMembers.add("Trirattree Koanamsai 6220503210"); //TEST
         groupMembers.add("Nakrob Sukjai 6220502141");
         groupMembers.add("Pedklah Kamolsukyunyong 6220504721");
         groupMembers.add("Pakkawat Boonsri 6220504747");
