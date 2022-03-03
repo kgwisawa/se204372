e97dda0a1f64e82cc0bc5cdc7dab289a2646c93b
Trirattree <trirattree.k@ku.th>
2022-03-04 04:24:21 +0700

    home

--- se_project_code/views/pages/home.php
+++ se_project_code/views/pages/home.php
@@ -4,7 +4,9 @@
 
 <body>
     <!-- Flickity HTML init -->
-    <div class="carousel" data-flickity='{ "autoPlay": true,"freeScroll": true,"wrapAround": true}'>
+    <input type="search">
+    <div class="carousel" data-flickity='{ "autoPlay": true,"freeScroll": true,"wrapAround": true,
+        "autoPlay": 1500,"prevNextButtons": false}'>
         <div class="carousel-cell"><br>kuy</br></div>
         <div class="carousel-cell"><br>kuy2</br></div>
         <div class="carousel-cell"><br>kuy3</br></div>
@@ -12,6 +14,13 @@
         <div class="carousel-cell"><br>kuy5</br></div>
     </div>
     <h2>News</h2>
+    <div class="carousel" data-flickity='{ "autoPlay": true,"freeScroll": true,"wrapAround": true,
+        "autoPlay": 1500,"prevNextButtons": false}'>
+        <div class="carousel-cellimg"><br>kuy</br></div>
+        <div class="carousel-cellimg"><br>kuy</br></div>
+        <div class="carousel-cellimg"><br>kuy</br></div>
+        <div class="carousel-cellimg"><br>kuy</br></div>
+    </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 </body>
