29b936ca91706dbeb350433375f7e5e8387ee949
Trirattree <trirattree.k@ku.th>
2022-03-04 04:58:19 +0700

    home

--- se_project_code/views/pages/home.php
+++ se_project_code/views/pages/home.php
@@ -8,16 +8,17 @@
         <input class="search" placeholder="Search..." type="text">
     </div>
     <!-- Flickity HTML init -->
-    <div class="carousel" data-flickity='{ "autoPlay": true,"freeScroll": true,"wrapAround": true,
-        "autoPlay": 1500,"prevNextButtons": false}'>
+    <div class="carousel" data-flickity='{ "freeScroll": true,"wrapAround": true,"prevNextButtons": false,"pauseAutoPlayOnHover": false}'>
         <div class="carousel-cell"><br>kuy</br></div>
         <div class="carousel-cell"><br>kuy2</br></div>
         <div class="carousel-cell"><br>kuy3</br></div>
         <div class="carousel-cell"><br>kuy4</br></div>
         <div class="carousel-cell"><br>kuy5</br></div>
+        <div class="carousel-cell"><br>kuy6</br></div>
+        <div class="carousel-cell"><br>kuy7</br></div>
     </div>
     <h2>News</h2>
-    <div class="carousel" data-flickity='{ "autoPlay": true,"freeScroll": true,"wrapAround": true,"prevNextButtons": false}'>
+    <div class="carousel" data-flickity='{ "autoPlay": true,"freeScroll": true,"wrapAround": true,"prevNextButtons": false,"autoPlay": 2000}'>
         <div class="carousel-cellimg"><br>kuy</br></div>
         <div class="carousel-cellimg"><br>kuy</br></div>
         <div class="carousel-cellimg"><br>kuy</br></div>
