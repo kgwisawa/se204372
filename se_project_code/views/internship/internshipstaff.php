025131ab49536972a330185fca11333e2a98304f
Trirattree <trirattree.k@ku.th>
2022-03-05 03:46:24 +0700

    style table

--- se_project_code/views/internship/internshipstaff.php
+++ se_project_code/views/internship/internshipstaff.php
@@ -1,20 +1,33 @@
-<table border=1>
-    <tr>
-        <td>ID</td>
-        <td>Name</td>
-        <td>Company name</td>
-        <td>Position</td>
-        <td>Start Date</td>
-        <td>End Date</td>
-        <td>File</td>
-        <td>Status</td>
-        <td>Update status</td>
-    </tr>
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="styles_table.css">
 
-    <?php 
+<body>
+    <section>
+        <div class="tbl-header">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Company name</th>
+                        <th>Position</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>File</th>
+                        <th>Status</th>
+                        <th>Update status</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="tbl-content">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <tbody>
+                    <?php
 
-    foreach ($internship_List as $il) {
-        echo "
+                    foreach ($internship_List as $il) {
+                        echo "
         <tr> 
         <td>$il->lnid</td>
         <td>$il->lnname</td>
@@ -27,8 +40,29 @@
         <td>$il->confirm</td>
         </tr>
         ";
-    }
+                    }
 
 
-    echo "</table>";
-    ?>
+                    ?>
+            </table>
+            <script>
+                $(window).on("load resize ", function() {
+                    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+                    $('.tbl-header').css({
+                        'padding-right': scrollWidth
+                    });
+                }).resize();
+            </script>
+        </div>
+    </section>
+    <script>
+        $(window).on("load resize ", function() {
+            var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+            $('.tbl-header').css({
+                'padding-right': scrollWidth
+            });
+        }).resize();
+    </script>
+</body>
+
+</html>
