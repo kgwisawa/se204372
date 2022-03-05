6c9471792b6ab65219c5411956b336461deb863c
kgwisawa <wisawa.cho@ku.th>
2022-03-06 03:16:00 +0700

    Update index.js

---
+++ SE/server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database:"memo_care"
+})
+
+app.get('/company', (req,res) =>{
+    db.query("SELECT * FROM company", (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.listen('3001', () => {
+    console.log("start")
+})
