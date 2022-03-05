a5b79583816410dbdc47e845e3b997d8532eeecd
kgwisawa <wisawa.cho@ku.th>
2022-03-06 05:50:50 +0700

    Update App.js

--- SE/myweb/src/App.js
+++ SE/myweb/src/App.js
@@ -1,7 +1,7 @@
 import {React} from 'react'
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './view';
+import Home from './view/home';
 import About from './view/about';
 import Services from './view/home';
 import Contact from './view/home';
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
       <Routes>
       <Route path='/' exact component={Home} />
-        <Route path='/about' component={About} />
+        <Route path='/about'><About/></Route>
         <Route path='/services' component={Services} />
         <Route path='/contact-us' component={Contact} />
         <Route path='/sign-up' component={SignUp} />
