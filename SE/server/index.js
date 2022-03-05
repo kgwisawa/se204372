4a2b7b5464531728d6575e7f8f708d57fbd98872
kgwisawa <wisawa.cho@ku.th>
2022-03-06 03:38:49 +0700

    Update index.js

--- SE/server/index.js
+++ SE/server/index.js
@@ -13,6 +13,10 @@ const db = mysql.createConnection({
     database:"memo_care"
 })
 
+
+//////////////////////////////////////////////////////////// Model Company
+
+//////////////////////////////////////////////////////////// read
 app.get('/company', (req,res) =>{
     db.query("SELECT * FROM company", (err, result) => {
         if(err) {
@@ -22,6 +26,50 @@ app.get('/company', (req,res) =>{
         }
     });
 });
+
+
+
+//////////////////////////////////////////////////////////// add
+app.post("/create/company", (req, res) => {
+    const id = req.body.id;
+    const name = req.body.name;
+    const link = req.body.link;
+
+    db.query(
+      "INSERT INTO company (cp_id, cp_name, cp_link) VALUES (?,?,?)",
+      [id, name, link],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Values Inserted");
+        }
+      }
+    );
+  });
+
+//////////////////////////////////////////////////////////// update
+
+  app.put("/update/company", (req, res) => {
+    const id = req.body.id;
+    const name = req.body.name;
+    const link = req.body.link;
+
+    db.query(
+      "UPDATE company SET (cp_name = ? , cp_link = ?) WHERE cp_link = ?",
+      [name, link ,id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
+
+//////////////////////////////////////////////////////////// Model Company end
+
 
 app.listen('3001', () => {
     console.log("start")
