6ae1a381cf072526f2cbb4a2d79dc37cc28dea2a
Trirattree <trirattree.k@ku.th>
2022-03-06 04:37:01 +0700

    hh

--- SE/myweb/src/App.js
+++ SE/myweb/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Container>
     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="Home">Home</Nav.Link>
+      <Nav.Link href="#Home">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
