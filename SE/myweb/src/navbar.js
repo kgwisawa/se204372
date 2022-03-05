cc8e29e47acd31eef62ea623027165f3db955bd4
kgwisawa <wisawa.cho@ku.th>
2022-03-06 03:58:25 +0700

    Update navbar.js

--- SE/myweb/src/navbar.js
+++ SE/myweb/src/navbar.js
@@ -1,19 +1,43 @@
 import React from 'react'
-import {Navbar , Container}from 'react-bootstrap'
+import {Navbar , Container ,Nav}from 'react-bootstrap'
 
 function navbar() {
   return (
-    <Navbar>
+<>
+  <Navbar bg="dark" variant="dark">
     <Container>
-      <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-          Signed in as: <a href="#login">Mark Otto</a>
-        </Navbar.Text>
-      </Navbar.Collapse>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
     </Container>
   </Navbar>
+  <br />
+  <Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+  <br />
+  <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+</>
   )
 }
 
