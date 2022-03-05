1b3d1c517e1af079b1a786852c67a70f29ee18c0
Trirattree <trirattree.k@ku.th>
2022-03-06 03:39:20 +0700

    home

--- SE/myweb/src/App.js
+++ SE/myweb/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [companyList, setCompanyList] = useState([]);
+  const getCompany = () => {
+    Axios.get("http://localhost:3001/company").then((response) => {
+      setCompanyList(response.data);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Kuy3232 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>Test</h1>
+      <div className="information">
+        <br />
+        <br />
+        <button class="btn btn-primary" onClick={getCompany}>
+          Show company
+        </button>
+        {companyList.map((val, key) => {
+          return (
+            <div className="employee card">
+              <div className="card-body text-left">
+                <p className="card-text">id: {val.cp_id}</p>
+                <p className="card-text">Name: {val.cp_name}</p>
+                <p className="card-text">link: {val.cp_link}</p>
+            </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
