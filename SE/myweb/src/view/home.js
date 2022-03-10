2b35a490bc39d1ef913215c5e97d24acc81f8f26
Trirattree <trirattree.k@ku.th>
2022-03-10 22:53:51 +0700

    home

--- SE/myweb/src/view/home.js
+++ SE/myweb/src/view/home.js
@@ -1,73 +1,96 @@
-import { React, useState } from "react";
+import { React, useState ,input} from "react";
 import Axios from "axios";
 import "../App.css";
 
 import Carousel from "react-elastic-carousel";
-// import InfiniteCarousel from 'react-leaf-carousel';
 import Item from "./Item";
+import ReactSearchBox from "react-search-box";
+
 const ip = "192.168.0.246";
 var start = 1;
 
 function Home() {
   const [companyList, setCompanyList] = useState([]);
   const getCompany = () => {
-    if(start === 1)
-    Axios.get("http://" + ip + ":3001/company").then((response) => {
-      setCompanyList(response.data);
-      start = 0;
-    });
-
+    if (start === 1)
+      Axios.get("http://" + ip + ":3001/company").then((response) => {
+        setCompanyList(response.data);
+        // alert(response.data)
+        start = 0;
+      });
   };
+  
 
   return (
+    
     <div className="App">
-    <div className="App">
+      <div className="App">
+        {/* <ReactSearchBox iconBoxSize="50px" placeholder="Search"  className="SearchBox"
+
+        /> */}
+        <br />
+        <br />
+        {getCompany()}
+        <Carousel
+          itemPadding={[10, 10]}
+          // pagination={false}
+          showArrows={false}
+          enableAutoPlay={true}
+          itemsToShow={4}
+          autoPlaySpeed={10000}
+          itemsToScroll={4}
+        >
+          {companyList.map((val, key) => {
+            return (
+              <Item>
+                {val.cp_name}
+                <div className="info">
+                  <a href={val.cp_link} target="_blank" className="bt">
+                    Info
+                  </a>
+                </div>
+              </Item>
+            );
+          })}
+        </Carousel>
+      </div>
       <br />
-      {getCompany()}
+      <br />
+      <h3>News</h3>
       <Carousel
         itemPadding={[10, 10]}
-        pagination={false}
-        showArrows={false}
-        enableAutoPlay={true}
-        itemsToShow={4}
-        autoPlaySpeed={10000}
-        itemsToScroll={4}
-      >
-        {companyList.map((val, key) => {
-          return (
-            // <div className="employee card">
-            //   <div className="card-body text-left">
-            //     <p className="card-text">id: {val.cp_id}</p>
-            //     <p className="card-text">Name: {val.cp_name}</p>
-            //     <p className="card-text">link: {val.cp_link}</p>
-            // </div>
-            // </div>
-            <Item>{val.cp_name}</Item>
-          );
-        })}
-      </Carousel>
-    </div>
-    <br/>
-    <br/>
-    <h3>News</h3>
-    <Carousel
-        itemPadding={[10, 10]}
-        pagination={false}
+        // pagination={false}
         showArrows={false}
         enableAutoPlay={true}
         itemsToShow={1}
         autoPlaySpeed={20000}
         itemsToScroll={1}
       >
-      <Item><h1>k1</h1></Item>
-      <Item><h1>k2</h1></Item>
-      <Item><h1>k3</h1></Item>
-      <Item><h1>k4</h1></Item>
-      <Item><h1>k5</h1></Item>
-      <Item><h1>k6</h1></Item>
-      <Item><h1>k7</h1></Item>
-      <Item><h1>k8</h1></Item>
-    </Carousel>
+        <Item>
+          <h1>k1</h1>
+        </Item>
+        <Item>
+          <h1>k2</h1>
+        </Item>
+        <Item>
+          <h1>k3</h1>
+        </Item>
+        <Item>
+          <h1>k4</h1>
+        </Item>
+        <Item>
+          <h1>k5</h1>
+        </Item>
+        <Item>
+          <h1>k6</h1>
+        </Item>
+        <Item>
+          <h1>k7</h1>
+        </Item>
+        <Item>
+          <h1>k8</h1>
+        </Item>
+      </Carousel>
     </div>
   );
 }
