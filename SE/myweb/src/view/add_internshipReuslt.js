af5302c2339e990bc5a52f1e56e2b569ed661b67
MIIKI <nakrob.s@ku.th>
2022-03-18 03:29:27 +0700

    finish

--- SE/myweb/src/view/add_internshipReuslt.js
+++ SE/myweb/src/view/add_internshipReuslt.js
@@ -3,7 +3,7 @@ import "../style/add_internship.css";
 import "react-dropdown/style.css";
 // import { Checkbox } from 'react-native-paper';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link, useParams , useNavigate  } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import Axios from "axios";
 import Dropdown from "react-dropdown";
 import Moment from "react-moment";
@@ -13,21 +13,19 @@ import "moment-timezone";
 const userData = [{ name: "ฝึกงาน" }, { name: "สหกิจศึกษา" }];
 
 function Add_internship() {
-
   const navigate = useNavigate();
 
-
   const { id } = useParams();
-  const start = moment().format("l");
+  const start = moment().format("DD/MM/YYYY");
   const [company, setcompany] = useState([]);
   const [companyList, setCompanyList] = useState([]);
 
   function setcp(data) {
     // alert(companyList[0].cp_name );
-    for(let i in companyList){
-      if(companyList[i].cp_name === data){
-          setcp_id(companyList[i].cp_id);
-          // alert(companyList[i].cp_id)
+    for (let i in companyList) {
+      if (companyList[i].cp_name === data) {
+        setcp_id(companyList[i].cp_id);
+        // alert(companyList[i].cp_id)
       }
     }
   }
@@ -66,9 +64,6 @@ function Add_internship() {
 
   const [internship, setinternship] = useState([]);
 
-
-
-
   const [users, setUsers] = useState([]);
   const [baseImage, setBaseImage] = useState("");
   const uploadImage = async (e) => {
@@ -95,38 +90,27 @@ function Add_internship() {
   const ip = "192.168.0.239";
 
   const addinternship = () => {
-    Axios.post("http://" + ip + ":3001/create/internshipdocument", {
+    Axios.post("http://" + ip + ":3001/create/internshipresult", {
       ln_id: ln_id,
-      id_date: id_date,
+      ir_date: id_date,
       cp_id: cp_id,
-      id_position: id_position,
-      id_sdate: id_sdate,
-      id_edate: id_edate,
-      id_file: baseImage,
-      id_status: id_status,
-      id_confirm: id_confirm,
-      id_year: id_year,
+      ir_image: baseImage,
+      ir_status: id_status,
     }).then(() => {
       setinternship([
         ...internship,
         {
           ln_id: ln_id,
-          id_date: id_date,
+          ir_date: id_date,
           cp_id: cp_id,
-          id_position: id_position,
-          id_sdate: id_sdate,
-          id_edate: id_edate,
-          id_file: baseImage,
-          id_status: id_status,
-          id_confirm: id_confirm,
-          id_year: id_year,
+          ir_image: baseImage,
+          ir_status: id_status,
         },
       ]);
     });
 
     alert("submit successful");
-    navigate("/internship/"+id);
-
+    navigate("/internship/" + id);
   };
 
   useEffect(() => {
@@ -165,7 +149,7 @@ function Add_internship() {
             </div>
           ))}
         </div>
-        <br/>
+        <br />
         {/* <div className="txt_field-in">
           <input
             type={"text"}
@@ -183,7 +167,7 @@ function Add_internship() {
             options={setcom()}
             placeholder="Select Company"
             onChange={(event) => {
-             setcp(event.value);
+              setcp(event.value);
             }}
           />
         </div>
@@ -226,7 +210,7 @@ function Add_internship() {
           <span></span>
           <label>Year</label>
         </div> */}
-        <br/>
+        <br />
         <div>
           <input
             type="file"
