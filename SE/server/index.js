49cef6667a3ae01a2b5fa942edf403c00528c087
PedklahZa007 <91517441+PedklahZa007@users.noreply.github.com>
2022-03-19 22:32:33 +0700

    Update index.js

--- SE/server/index.js
+++ SE/server/index.js
@@ -93,7 +93,7 @@ app.post("/create/company", (req, res) => {
 //*?----------------------------------------------------------------------------------------- read
 
 app.get('/loginnisit', (req,res) =>{
-  db.query("SELECT ln_id ,email , password FROM loginnisit", (err, result) => {
+  db.query("SELECT ln_id ,email , password ,ln_name FROM loginnisit", (err, result) => {
       if(err) {
           console.log(err);
       } else {
@@ -135,7 +135,71 @@ app.get('/loginadmin', (req,res) =>{
 //*?-----------------------------------------------------------------------------------------  read
 
 app.get('/internshipdocument', (req,res) =>{
-  db.query("SELECT  id_id , ln_id , ln_name , email  ,id_date ,  cp_name  , `id_position`, `id_sdate`,`id_edate`,`id_file`,`id_status`,`id_confirm` FROM internshipDocument JOIN company  USING(cp_id) JOIN loginNisit USING(ln_id)", (err, result) => {
+  db.query("SELECT  id_comment,id_id , ln_id , ln_name ,id_year , email  ,id_date ,  cp_name  , `id_position`, `id_sdate`,`id_edate`,`id_file`,`id_status`,`id_confirm` FROM internshipDocument JOIN company  USING(cp_id) JOIN loginNisit USING(ln_id)", (err, result) => {
+      if(err) {
+          console.log(err);
+      } else {
+          res.send(result);
+      }
+  });
+});
+
+
+//*?-----------------------------------------------------------------------------------------  read nisit name id
+
+app.get('/internshipdocument', (req,res) =>{
+  db.query("SELECT ln_id , ln_name FROM internshipDocument JOIN loginNisit USING(ln_id)", (err, result) => {
+      if(err) {
+          console.log(err);
+      } else {
+          res.send(result);
+      }
+  });
+});
+
+
+
+//*?-----------------------------------------------------------------------------------------  read orderstatus
+
+app.get('/internshipdocument/orderstatus', (req,res) =>{
+  db.query("SELECT  id_comment,id_id , ln_id , ln_name ,id_year , email  ,id_date ,  cp_name  , `id_position`, `id_sdate`,`id_edate`,`id_file`,`id_status`,`id_confirm` FROM internshipDocument JOIN company  USING(cp_id) JOIN loginNisit USING(ln_id) ORDER BY id_status DESC", (err, result) => {
+      if(err) {
+          console.log(err);
+      } else {
+          res.send(result);
+      }
+  });
+});
+
+//*?-----------------------------------------------------------------------------------------  read orderdate
+
+app.get('/internshipdocument/orderdate', (req,res) =>{
+  db.query("SELECT  id_comment,id_id , ln_id , ln_name ,id_year , email  ,id_date ,  cp_name  , `id_position`, `id_sdate`,`id_edate`,`id_file`,`id_status`,`id_confirm` FROM internshipDocument JOIN company  USING(cp_id) JOIN loginNisit USING(ln_id) ORDER BY id_id DESC", (err, result) => {
+      if(err) {
+          console.log(err);
+      } else {
+          res.send(result);
+      }
+  });
+});
+
+//*?-----------------------------------------------------------------------------------------  read orderyear
+
+app.get('/internshipdocument/orderyear', (req,res) =>{
+  db.query("SELECT  id_comment,id_id , ln_id , ln_name ,id_year , email  ,id_date ,  cp_name  , `id_position`, `id_sdate`,`id_edate`,`id_file`,`id_status`,`id_confirm` FROM internshipDocument JOIN company  USING(cp_id) JOIN loginNisit USING(ln_id) ORDER BY id_year DESC", (err, result) => {
+      if(err) {
+          console.log(err);
+      } else {
+          res.send(result);
+      }
+  });
+});
+
+//*?-----------------------------------------------------------------------------------------  search
+
+app.get('/internshipdocument/search', (req,res) =>{
+  const key = req.body.key;
+  db.query("SELECT   * FROM internshipDocument JOIN company  USING(cp_id) JOIN loginNisit USING(ln_id) where ln_name like '%?%' or cp_name like '%?%' or id_status like '%?%'", [key,key,key],(err, result) => {
       if(err) {
           console.log(err);
       } else {
@@ -148,6 +212,7 @@ app.get('/internshipdocument', (req,res) =>{
 app.post("/create/internshipdocument", (req, res) => {
   const ln_id = req.body.ln_id;
   const id_date = req.body.id_date;
+  const id_year = req.body.id_year;
   const cp_id = req.body.cp_id;
   const id_position = req.body.id_position;
   const id_sdate = req.body.id_sdate;
@@ -159,8 +224,8 @@ app.post("/create/internshipdocument", (req, res) => {
   const id_confirm = req.body.id_confirm;
 
   db.query(
-    "INSERT INTO internshipdocument (ln_id  , id_date ,  cp_id  , `id_position`, `id_sdate`,`id_edate`,`id_file`,`id_tfile1`,`id_tfile2`,`id_status`,`id_confirm`) VALUES (?,?,?,?,?,?,?,?,?,?,?)",
-    [ln_id , id_date, cp_id ,id_position,id_sdate,id_edate,id_file ,id_tfile1,id_tfile2, id_status, id_confirm],
+    "INSERT INTO internshipdocument (ln_id  , id_date  , cp_id , id_year  , `id_position`, `id_sdate`,`id_edate`,`id_file`,`id_tfile1`,`id_tfile2`,`id_status`,`id_confirm`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
+    [ln_id , id_date, cp_id,id_year ,id_position,id_sdate,id_edate,id_file ,id_tfile1,id_tfile2, id_status, id_confirm],
     (err, result) => {
       if (err) {
         console.log(err);
@@ -176,11 +241,12 @@ app.post("/create/internshipdocument", (req, res) => {
 app.put("/update/internshipdocument", (req, res) => {
   const id = req.body.id;
   const status = req.body.status;
+  const id_comment = req.body.id_comment;
 
 
   db.query(
-    "UPDATE internshipdocument SET id_status = ? WHERE id_id = ?",
-    [ status ,id],
+    "UPDATE internshipdocument SET id_status = ? , id_comment = ? WHERE id_id = ?",
+    [ status ,id_comment ,id],
     (err, result) => {
       if (err) {
         console.log(err);
@@ -211,16 +277,38 @@ app.get('/internshipresult', (req,res) =>{
   });
 });
 
+//*?-----------------------------------------------------------------------------------------  add
+app.post("/create/internshipresult", (req, res) => {
+  const ln_id = req.body.ln_id;
+  const cp_id = req.body.cp_id;
+  const ir_date = req.body.ir_date;
+  const ir_status = req.body.ir_status;
+  const ir_image = req.body.ir_image;
+
+  db.query(
+    "INSERT INTO internshipresult (ln_id,cp_id ,ir_date,ir_status,ir_image) VALUES (?,?,?,?,?)",
+    [ln_id,cp_id,ir_date, ir_status, ir_image],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
 //////////////////////////////////////////////////////////// update
 
 app.put("/update/internshipresult", (req, res) => {
   const id = req.body.id;
+  const detail = req.body.detail;
   const status = req.body.status;
 
 
   db.query(
-    "UPDATE internshipresult SET ir_status = ? WHERE ir_id = ?",
-    [ status ,id],
+    "UPDATE internshipresult SET ir_status = ?, ir_detail = ? WHERE ir_id = ?",
+    [ status ,detail,id],
     (err, result) => {
       if (err) {
         console.log(err);
